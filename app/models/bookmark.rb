bc6389e9f2aea76ba76cc5a770d2f603eeb6f52f
sofia ngapani <withsofina@gmail.com>
2022-08-19 14:12:14 +0800

    watch list

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: {scope: :list_id, message: "what is the movie?"}
+
 end
